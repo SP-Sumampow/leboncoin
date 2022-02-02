0ec3dbfb098b53e6bc79e5ead150543d1d4176a8
Anthony Roux <anthony.roux@colorz.fr>
2022-02-02 13:31:18 +0100

    add bootstrap

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -10,3 +10,18 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+// https://symfony.com/doc/current/frontend/encore/bootstrap.html
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
