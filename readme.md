4f2e3979157ee0e821c7f0205a8864aa3fed81f1
SP-Sumampow <vivian.priska@gmail.com>
2022-02-04 20:43:03 +0100

    add info

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Le bon coins du pauvre
+
+Projet école PHP web 3
+
+Pour lancer l'infra sur docker [Ngninx, PHP + Node.JS, MYSQL]
+> sh start.sh
+>
+Le password des users dans les fixtures est `hackme`
--- symfony/src/Controller/UserController.php
+++ symfony/src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\TagRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,20 +27,37 @@ class UserController extends AbstractController
      */
     public function buyAds(TagRepository $tagRepository): Response
     {
+        $user = $this->getUser();
+
+        if (null === $user || !$user instanceof User) {
+            // do something about the wrong types, that you might get from getSecurity()->getUser(), e.g. return or throw an exception
+            throw Exception(sprintf('Expected App\\Entity\\User, got %s', $user === null ? 'null' : get_class($user)));
+        }
+
         $tags = $tagRepository->findAll();
-        return $this->render('user/buyAds.html.twig', [
-            'tags' => $tags
+        return $this->render('user/sellAds.html.twig', [
+            'tags' => $tags,
+            'ads' => $user->getBuyAds()
         ]);
     }
 
     /**
      * + @Route("/user/sellAds", "sell_ads")
      */
-    public function sellAds(TagRepository $tagRepository): Response
+    public function sellAds(TagRepository $tagRepository, UserRepository $userRepository): Response
     {
+        $user = $this->getUser();
+
+        if (null === $user || !$user instanceof User) {
+            // do something about the wrong types, that you might get from getSecurity()->getUser(), e.g. return or throw an exception
+            throw Exception(sprintf('Expected App\\Entity\\User, got %s', $user === null ? 'null' : get_class($user)));
+        }
+
         $tags = $tagRepository->findAll();
         return $this->render('user/sellAds.html.twig', [
             'tags' => $tags,
+            'ads' => $user->getSellAds()
         ]);
     }
+
 }
