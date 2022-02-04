a05daf66c6fa914dab438265bc7f18a7287e3f13
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2022-02-04 19:18:33 +0100

    add fixtures products

--- symfony/src/DataFixtures/AdFixtures.php
+++ symfony/src/DataFixtures/AdFixtures.php
@@ -14,6 +14,8 @@ class AdFixtures extends Fixture implements DependentFixtureInterface
     public const AD_2_RED_DRESS = 'msg_ad_2_red_dress';
     public const AD_2_TSHIRT = 'msg_ad_tshirt';
     public const AD_SYMFONYBOOK = 'msg_ad_symfony_book';
+    public const AD_CLEANCODEBOOK = 'msg_ad_cleancode_book';
+    public const AD_NEWBALANCESHOES = 'msg_ad_newbalance_shoes';
 
     public function load(ObjectManager $manager)
     {   
@@ -33,18 +35,60 @@ class AdFixtures extends Fixture implements DependentFixtureInterface
         $this->addReference(AdFixtures::AD_SYMFONYBOOK, $symfonyBookAd);
         $manager->persist($symfonyBookAd);
 
+        $cleanCodeBookAd = $this->generateAvailableAdCleanBook();
+        $this->addReference(AdFixtures::AD_CLEANCODEBOOK, $cleanCodeBookAd);
+        $manager->persist($cleanCodeBookAd);
+
+        $newBalanceAd = $this->generateAvailableAdShoes();
+        $this->addReference(AdFixtures::AD_NEWBALANCESHOES, $newBalanceAd);
+        $manager->persist($newBalanceAd);
+
         $manager->flush();
+    }
+
+    public function generateAvailableAdShoes(): Ad
+    {
+        $ad6 = new Ad();
+        $ad6->setTitle("new balance");
+        $ad6->setSeller($this->getReference("seller2"));
+        $ad6->addTag($this->getReference(TagFixtures::TAG_SHOES));
+        $ad6->setReview($this->getReference(ReviewFixtures::REVIEW_AD_6));
+        $ad6->setPostalCode("93100");
+        $ad6->setPrice(7000);
+        $ad6->setDescription("New balance, taille 39 , neuf");
+        $ad6->setCurrency("EUR");
+        $ad6->setValidationCode("AB69EF");
+        $ad6->setStatus(AdStatusType::AVAILABLE->value);
+        $ad6->setCurrency("EUR");
+        return $ad6;
+    }
+
+    public function generateAvailableAdCleanBook(): Ad
+    {
+        $ad5 = new Ad();
+        $ad5->setTitle("clean code book");
+        $ad5->setSeller($this->getReference("seller3"));
+        $ad5->addTag($this->getReference(TagFixtures::TAG_BOOK));
+        $ad5->setReview($this->getReference(ReviewFixtures::REVIEW_AD_5));
+        $ad5->setPostalCode("93100");
+        $ad5->setPrice(1000);
+        $ad5->setDescription("Clean code book comme neuf");
+        $ad5->setCurrency("EUR");
+        $ad5->setValidationCode("AB69EF");
+        $ad5->setStatus(AdStatusType::AVAILABLE->value);
+        $ad5->setCurrency("EUR");
+        return $ad5;
     }
 
     public function generateAvailableAdSymfonyBook(): Ad
     {
         $ad4 = new Ad();
-        $ad4->setTitle("syfont book");
+        $ad4->setTitle("symfony book");
         $ad4->setSeller($this->getReference("seller3"));
         $ad4->addTag($this->getReference(TagFixtures::TAG_BOOK));
         $ad4->setReview($this->getReference(ReviewFixtures::REVIEW_AD_4));
         $ad4->setPostalCode("93100");
-        $ad4->setPrice(6000);
+        $ad4->setPrice(1200);
         $ad4->setDescription("Symfony book comme neuf");
         $ad4->setCurrency("EUR");
         $ad4->setValidationCode("AB69EF");
--- symfony/src/DataFixtures/MediaFixtures.php
+++ symfony/src/DataFixtures/MediaFixtures.php
@@ -12,8 +12,11 @@ class MediaFixtures extends Fixture implements DependentFixtureInterface
 {
     public const MEDIA_AD_1_IPHONE13_1 = 'photo_ad1_iphone13_1';
     public const MEDIA_AD_1_IPHONE13_2 = 'photo_ad1_iphone13_2';
-
     public const MEDIA_AD_DRESS = 'photo_dress_1';
+    public const MEDIA_AD_CLEANCODE_BOOK = 'photo_cleancode_book';
+    public const MEDIA_AD_SYMFONY_BOOK = 'photo_symfony_book';
+    public const MEDIA_AD_ADIDAS_TSHIRT = 'photo_adidas_tshirt';
+    public const MEDIA_AD_NEWBALANCE_SHOES = 'photo_newbalance_shoes';
 
     public function load(ObjectManager $manager)
     {
@@ -37,6 +40,34 @@ class MediaFixtures extends Fixture implements DependentFixtureInterface
         $this->addReference(MediaFixtures::MEDIA_AD_DRESS, $media3);
         $media3->setAd($this->getReference(AdFixtures::AD_2_RED_DRESS));
         $manager->persist($media3);
+
+        $media4 = new Media();
+        $media4->setType(MediaType::PICTURE->value);
+        $media4->setUrl("https://firebasestorage.googleapis.com/v0/b/testo--backoffice.appspot.com/o/cleanCodeBook.jpg?alt=media&token=50e597ad-ac1f-44f5-b203-1725b48b766b");
+        $this->addReference(MediaFixtures::MEDIA_AD_CLEANCODE_BOOK, $media4);
+        $media4->setAd($this->getReference(AdFixtures::AD_CLEANCODEBOOK));
+        $manager->persist($media4);
+
+        $media5 = new Media();
+        $media5->setType(MediaType::PICTURE->value);
+        $media5->setUrl("https://firebasestorage.googleapis.com/v0/b/testo--backoffice.appspot.com/o/symfony5-book-cover.png?alt=media&token=7af6cbea-1e3b-4931-87ca-b710e02a6cc6");
+        $this->addReference(MediaFixtures::MEDIA_AD_SYMFONY_BOOK, $media5);
+        $media5->setAd($this->getReference(AdFixtures::AD_SYMFONYBOOK));
+        $manager->persist($media5);
+
+        $media6 = new Media();
+        $media6->setType(MediaType::PICTURE->value);
+        $media6->setUrl("https://firebasestorage.googleapis.com/v0/b/testo--backoffice.appspot.com/o/adidasTshirt.jpeg?alt=media&token=a74292cf-1b43-46bb-b795-603fee0e1d17");
+        $this->addReference(MediaFixtures::MEDIA_AD_ADIDAS_TSHIRT, $media6);
+        $media6->setAd($this->getReference(AdFixtures::AD_2_TSHIRT));
+        $manager->persist($media6);
+
+        $media7 = new Media();
+        $media7->setType(MediaType::PICTURE->value);
+        $media7->setUrl("https://firebasestorage.googleapis.com/v0/b/testo--backoffice.appspot.com/o/newBalance327.jpeg?alt=media&token=6b3a8eda-f838-4058-ae99-cc81532fef4d");
+        $this->addReference(MediaFixtures::MEDIA_AD_NEWBALANCE_SHOES, $media7);
+        $media7->setAd($this->getReference(AdFixtures::AD_NEWBALANCESHOES));
+        $manager->persist($media7);
 
         $manager->flush();
     }
